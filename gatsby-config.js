424db67464f8b99d160eeb0099eda215c50038ce
Jason Mischka <jasonmischka@Jasons-MBP.fios-router.home>
2021-05-11 17:49:24 -0400

    config fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-sanity',
       options: {
